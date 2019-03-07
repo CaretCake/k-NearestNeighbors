72b4f727e72866cf3e6df3fd8a79fada73becd90
CaretCake <caretcake@outlook.com>
2019-03-07 10:46:02 -0500

    Implemented train() and test()

--- KNNModel.java
+++ KNNModel.java
@@ -20,15 +20,37 @@ public class KNNModel {
 
   public void test(String filename, int neighbors) {
     // parse data from filename
-    // for every instance in filename data
-      // create a queue of some sort to store k closest neighbors
-      // for every instance in stored data
-        // check euclidean distance from test data instance
-        // if distance to stored data instance < any in queue
-          // add stored data instance to queue of closest neighbors
-          // remove furthest distance stored data instance from queue
-      // predict target feature value based on closest neighbors queue
-    // calculate accuracy + print it (root mean squared error if target is continuous)
+    try {
+      ArffParser parser = new ArffParser();
+      parser.setInputFilename(filename);
+      parser.parseDataFromArffFile();
+      ArrayList<Feature> testFeatures = parser.getFeatures();
+    	ArrayList<ArrayList<Double>> testDataInstances = parser.getDataInstances();
+
+      // for every instance in filename data
+      for (ArrayList<Double> testInstance : testDataInstances) {
+        // create a queue of some sort to store k closest neighbors
+        ArrayList<Double> distances = new ArrayList<Double>();
+        // for every instance in stored data
+        for (ArrayList<Double> instance : dataInstances) {
+          // check euclidean distance from test data instance
+          double distance = 0;
+          for (int i = 0; i < instance.size(); i++) {
+            distance += Math.pow(testInstance.get(i) - instance.get(i), 2);
+          }
+          distance = Math.sqrt(distance);
+          distances.add(distance);
+          System.out.println(distance);
+          // if distance to stored data instance < any in queue
+            // add stored data instance to queue of closest neighbors
+            // remove furthest distance stored data instance from queue
+        }
+        // predict target feature value based on closest neighbors queue
+      }
+      // calculate accuracy + print it (root mean squared error if target is continuous)
+    } catch (Exception e) {
+      System.out.println(e);
+    } 
   }
 
 }
