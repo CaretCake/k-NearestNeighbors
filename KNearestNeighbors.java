2c3a2980e6ace6cdeafafe0b6666d4ce42b8d976
CaretCake <caretcake@outlook.com>
2019-03-10 20:23:55 -0400

    Added check for 0 or negative number of neighbors input

--- KNearestNeighbors.java
+++ KNearestNeighbors.java
@@ -5,6 +5,11 @@ public class KNearestNeighbors {
     String testFilename = args[1];
     int numOfNeighbors = Integer.parseInt(args[2]);
 
+    if (numOfNeighbors < 1) {
+      System.out.println("Cannot use less than 1 neighbor.");
+      System.exit(0);
+    }
+
     KNNModel kNNModel = new KNNModel();
 
     kNNModel.train(trainFilename);
